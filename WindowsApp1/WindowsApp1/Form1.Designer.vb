eed02ecff08e1e72365e65fbb811f7f1a96b91cc
pandelaz <pandelaz@gmail.com>
2017-10-13 23:08:32 +0800

    PCB&VB

---
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,315 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(308, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(160, 54)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(83, 147)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 24)
+        Me.Label1.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(183, 210)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(219, 36)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(80, 218)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 24)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "wifi pwd"
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(87, 257)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(320, 53)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "修改"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 58)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(121, 24)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "pisok valuse"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(20, 106)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(104, 24)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "pisok time"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(20, 153)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(140, 24)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "emgisok value"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(172, 55)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(167, 36)
+        Me.TextBox4.TabIndex = 11
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(172, 103)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(167, 36)
+        Me.TextBox5.TabIndex = 12
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(172, 150)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(167, 36)
+        Me.TextBox6.TabIndex = 13
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(87, 405)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(320, 53)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "login"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(83, 364)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(110, 24)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "admin pwd"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(206, 352)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox7.Size = New System.Drawing.Size(196, 36)
+        Me.TextBox7.TabIndex = 16
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(24, 204)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(320, 53)
+        Me.Button4.TabIndex = 17
+        Me.Button4.Text = "修改"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("新細明體", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label8.Location = New System.Drawing.Point(12, 44)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(132, 37)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Status : "
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("新細明體", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label9.Location = New System.Drawing.Point(145, 45)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(128, 37)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "已斷線"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(83, 171)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 24)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "wifi ssid"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(183, 168)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(219, 36)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(183, 126)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(219, 36)
+        Me.TextBox8.TabIndex = 22
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(83, 129)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(34, 24)
+        Me.Label10.TabIndex = 21
+        Me.Label10.Text = "ID"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.TextBox5)
+        Me.GroupBox1.Controls.Add(Me.TextBox6)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Location = New System.Drawing.Point(67, 480)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(363, 272)
+        Me.GroupBox1.TabIndex = 23
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "admin "
+        Me.GroupBox1.Visible = False
+        '
+        'Timer2
+        '
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 8000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(493, 787)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+End Class
---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports System.IO.Ports
+
+Public Class Form1
+
+    Dim connected As Boolean = False
+    Dim tmp As String = ""
+    Dim errcnt As Integer = 0
+    Dim inbyte As Integer = 8
+    Dim totalinfo As String = ""
+    Dim sernum As Integer = 0
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If TextBox7.Text = "ntutkenny" Then
+            GroupBox1.Visible = True
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SerialPort1.Write(TextBox8.Text & vbCrLf &
+                          TextBox2.Text & vbCrLf &
+                          TextBox3.Text & vbCrLf &
+                          TextBox4.Text & vbCrLf &
+                          TextBox5.Text & vbCrLf &
+                          TextBox6.Text)
+
+        'Timer3.Enabled = True
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        SerialPort1.Write(TextBox8.Text & vbCrLf &
+                          TextBox2.Text & vbCrLf &
+                          TextBox3.Text & vbCrLf &
+                          TextBox4.Text & vbCrLf &
+                          TextBox5.Text & vbCrLf &
+                          TextBox6.Text)
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        If SerialPort1.BytesToRead > 0 Then
+            tmp = SerialPort1.ReadExisting()
+            If tmp.IndexOf("end") >= 0 Then
+                totalinfo &= tmp
+            Else
+                totalinfo &= tmp
+            End If
+        End If
+
+        If totalinfo.IndexOf("end") >= 0 Then
+            Dim a() As String = totalinfo.Split(vbLf)
+            Dim aa As Integer
+            For aa = 0 To a.Length - 1
+                a(aa) = a(aa).Replace(vbCr, "")
+                'Debug.Print(a(aa))
+            Next aa
+
+            TextBox8.Text = a(0)
+            TextBox2.Text = a(1)
+            TextBox3.Text = a(2)
+            TextBox4.Text = a(3)
+            TextBox5.Text = a(4)
+            TextBox6.Text = a(5)
+
+            Timer2.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+
+        If SerialPort1.BytesToRead > 0 Then
+            tmp = SerialPort1.ReadExisting()
+            Debug.Print(tmp)
+            Timer3.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim serarr() As String = SerialPort.GetPortNames
+
+
+
+        Dim i As Integer = 0
+
+        For i = 0 To serarr.Length - 1
+            SerialPort1.PortName = serarr(i)
+
+            errcnt = 0
+            SerialPort1.Parity = IO.Ports.Parity.None
+            SerialPort1.StopBits = IO.Ports.StopBits.One
+            SerialPort1.DataBits = 8
+            SerialPort1.BaudRate = 9600
+
+            Try
+                connected = False
+                totalinfo = ""
+                inbyte = 8
+                SerialPort1.Open()
+            Catch ex As Exception
+                Return
+            End Try
+
+            If SerialPort1.IsOpen() Then
+                SerialPort1.Write("computer")
+                Button1.Enabled = False
+
+                While (SerialPort1.BytesToRead < 8 And errcnt < 3000)
+                    errcnt += 1
+                    Threading.Thread.Sleep(1)
+                    Application.DoEvents()
+                End While
+
+                If SerialPort1.BytesToRead > 0 Then
+
+                    tmp = SerialPort1.ReadExisting()
+
+                    If tmp.IndexOf("connected") >= 0 Then
+                        connected = True
+                        Label9.Text = "已連線"
+                        Button1.Visible = False
+                        Timer1.Enabled = False
+                        Timer2.Enabled = True
+                        Button2.Enabled = True
+                        Button3.Enabled = True
+                        Button4.Enabled = True
+                        Exit Sub
+                    Else
+                        If SerialPort1.IsOpen() = True Then
+                            SerialPort1.Close()
+                        End If
+                    End If
+                End If
+            End If
+        Next i
+
+        MsgBox("請確定是否已連接", MsgBoxStyle.Critical, "Error")
+        Me.Dispose()
+
+    End Sub
+End Class
